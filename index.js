e1aa3768658c5b85f04ef0d8f381f453a97a8e09
khnn1234 <majidkhanh9@gmail.com>
2025-01-15 09:59:16 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+  function toggleMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+    }}
